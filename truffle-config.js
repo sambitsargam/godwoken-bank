54c6589d06578a886afe6a719bf0b7fa8c855963
SAMBIT SARGAM EKALABAYA <sambitsargam2003@gmail.com>
2022-05-21 23:28:17 +0530

    upload files

---
+++ truffle-config.js
@@ -0,0 +1,32 @@
+require('babel-register');
+require('babel-polyfill');
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    godwoken: {
+      provider: () => new HDWalletProvider(mnemonic, `https://godwoken-testnet-v1.ckbapp.dev/`),
+      network_id: 71401,
+    },
+  },
+
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      version: ">=0.6.0 <0.8.0",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
